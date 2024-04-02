03ed95ea1f07f57717ef2475c1da3a3a6c8912da
lilalr <gurennut@gmail.com>
2024-04-02 19:55:34 +0700

    buat fungsi utama

--- activitycrust keempat/activitycrust keempat.cpp
+++ activitycrust keempat/activitycrust keempat.cpp
@@ -14,3 +14,38 @@ struct Mahasiswa {
     string umur;
 };
 
+int main()
+{
+    Mahasiswa mhs[3];
+    for (int i = 0; i < 3; i++) {
+        cout << "data ke-" << (i + 1) << ":" << endl;
+        cout << "Nomor mahasiswa: ";
+        getline(cin, mhs[i].nim); // cin harus sesudah getline 
+        cout << "Nama Mahasiswa: ";
+        getline(cin, mhs[i].nama); // getline berfungsi saat penulisan mana lengkap
+
+        cout << "Alamat Mahasiswa: " << endl;
+        cout << "\t Nama Desa: ";
+        cin >> mhs[i].alamat.desa;
+        cout << "\t Nama Kota: ";
+        cin >> mhs[i].alamat.kota;
+
+        cout << "Umur mahasiswa: ";
+        cin >> mhs[i].umur;
+        cin.ignore(1, '\n');
+        cout << endl;
+    }
+
+    for (int i = 0; i < 3; i++) {
+
+        cout << endl;
+        cout << "Data mahasiswa ke-" << (i + 1) << ":" << endl;
+        cout << "\n NIM: " << mhs[i].nim;
+        cout << "\n Nama: " << mhs[i].nama;
+        cout << "\n Alamat: ";
+        cout << "\n \tDesa: " << mhs[i].alamat.desa;
+        cout << "\n \tKota: " << mhs[i].alamat.kota;
+        cout << "\n Umur: " << mhs[i].umur;
+        cout << endl;
+    }
+}
