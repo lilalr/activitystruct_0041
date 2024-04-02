1a40ddf6819ed418ffd660e1623eec579edd4031
lilalr <gurennut@gmail.com>
2024-04-02 18:20:02 +0700

    menggunakan perintah getline

--- activitycrust keempat/activitycrust keempat.cpp
+++ activitycrust keempat/activitycrust keempat.cpp
@@ -19,4 +19,8 @@ int main() {
 	for (int i = 0; i < 3; i++) {
 		cout << "data ke-" << (i + 1) << ":" << endl;
 		cout << "Nomor mahasiswa : ";
+		getline(cin, mhs[i].nim);
+		cout << "Nama Mahasiswa : ";
+		getline(cin, mhs[i].nama);
+
 		
